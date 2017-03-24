72d810f59508c1fd7b70bbc96572fa5971edfe1a
tictactimon <hut@Snoopy-6.local>
2017-03-24 16:55:43 +0100

    RC Version 1
    Kleiner Änderungen und Verbesserungen.

--- install.sql
+++ install.sql
@@ -5,7 +5,7 @@ INSERT INTO vnews_category (category_name)
 VALUES ('Keine Kategorie'); 
 
 INSERT INTO rex_redactor2_profiles (name, description, urltype, minheight, maxheight, characterlimit, toolbarfixed, shortcuts, redactor_plugins)
-VALUES ('vlead','Redactor Profil für vNews Lead','relative','0','150','150','1','1',''); 
+VALUES ('vlead','Redactor Profil für vNews Lead','relative','0','150','150','1','1','paragraph'); 
 
 INSERT INTO rex_url_generate (`table`, table_parameters) 
-VALUES ('1_xxx_vnews_article','{"1_xxx_vnews_article_field_1":"article_title","1_xxx_vnews_article_field_2":"","1_xxx_vnews_article_field_3":"","1_xxx_vnews_article_id":"id","1_xxx_vnews_article_restriction_field":"","1_xxx_vnews_article_restriction_operator":"=","1_xxx_vnews_article_restriction_value":"","1_xxx_vnews_article_url_param_key":"","1_xxx_vnews_article_seo_title":"article_title","1_xxx_vnews_article_seo_description":"article_lead","1_xxx_vnews_article_sitemap_add":"1","1_xxx_vnews_article_sitemap_frequency":"always","1_xxx_vnews_article_sitemap_priority":"1.0","1_xxx_vnews_article_sitemap_lastmod":"article_title","1_xxx_vnews_article_path_names":"","1_xxx_vnews_article_path_categories":"0","1_xxx_vnews_article_relation_field":""}'); 
+VALUES ('1_xxx_vnews_article','{"1_xxx_vnews_article_field_1":"article_title","1_xxx_vnews_article_field_2":"","1_xxx_vnews_article_field_3":"","1_xxx_vnews_article_id":"id","1_xxx_vnews_article_restriction_field":"","1_xxx_vnews_article_restriction_operator":"=","1_xxx_vnews_article_restriction_value":"","1_xxx_vnews_article_url_param_key":"","1_xxx_vnews_article_seo_title":"article_title","1_xxx_vnews_article_seo_description":"article_lead","1_xxx_vnews_article_sitemap_add":"1","1_xxx_vnews_article_sitemap_frequency":"always","1_xxx_vnews_article_sitemap_priority":"1.0","1_xxx_vnews_article_sitemap_lastmod":"","1_xxx_vnews_article_path_names":"","1_xxx_vnews_article_path_categories":"0","1_xxx_vnews_article_relation_field":""}'); 
--- pages/main.manual.php
+++ pages/main.manual.php
@@ -1,40 +1,54 @@
 <?php
 
-$content = '<h3>Tabellen pflegen</h3>';
-$content = '<p>Für dieses AddOn verfügen Sie folgende drei Tabellen:</p>
-<ul>
-	<li>Artikel</li>
-	<li>Kategorie</li>
-	<li>Autor</li>
-</ul>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
-<strong>Artikel</strong>
-<p>In der Artikel-Tabelle sind alle News-Einträge gespeichert, Sie können neue Tabellen erstellen sowie Einträge verwalten und löschen.</p>
-<p></p>
-<strong>Kategorie</strong>
-<p>Erstellen Sie neue Kategorien und Sortieren Sie ihre Neuigkeiten nach Kategorien. Sie können einen Eintrag in der Artikel-Tabelle einer Kategorie zuordnen</p>
-<p></p>
-<strong>Autor</strong>
-<p>Erstellen Sie neue Autoren und ordnen Sie jedem Artikel einen Autor zu. Sie können jeden Eintrag in der Artikel-Tabelle einem Autor zuordnen</p>
-<p></p>
-<p></p>
-<strong>vNews aufsetzen</strong>
-<p>Als Sie vNews installierten, mussten gewisse AddOns vorinstalliert sein um dieses AddOn überhaupt nutzen zu können. Vergewissern Sie sich, dass alle benötigten AddOns immer noch installiert sind und auch einwandfrei funktionieren. Ebenso muss das AddOn "YRewrite" bereit aufgesetzt sein und einwandfrei funktionieren.</p>
-<p>Legen Sie fest, in welchem Artikel Sie die Artikelliste und in welchemer Artikel Sie die Detailansicht platzieren wollen. Erstellen Sie wenn nötig diese Artikel und fügen die AddOns ein.</p>
-<p></p>
-<p>Im AddOn "Artikelliste" wählen Sie im Feld "Detailansicht-Artikel" den Artikel aus, in dem das Modul "Detailansicht" platziert wurde.</p>
-<p>Wenn Sie den Artikel verlinkt haben, müssen Sie den gleichen Artikel nochmals im URL-AddOn. Wählen Sie dafür im URL-AddOn das bereits vom "vNews" erstelle Profil aus und wählen Sie im ersten Feld den gleichen Artikel aus wie im vorherigen Prozess. </p>
-<p>Wurden all diese Einstellungen getätigt können Sie beginnen Artikel zu erstellen für das News AddOn.</p>
-<p></p>
-<p></p>
-<p></p>';
+$content = '';
+
+$content .= '<section class="rex-page-section">
+             <div class="panel panel-default">
+    			<header class="panel-heading"><div class="panel-title">Tabellen</div></header>
+         			<div class="panel-body">
+					<p>Diese AddOn verfügt über folgende 3 Tabellen</p>
+					<ul>
+						<li>Artikel</li>
+						<li>Kategorie</li>
+						<li>Autor</li>
+					</ul>
+					<p>Die Felder in der Tabelle können beliebig gelöscht, ergänzt und verändert werden. Wichtig das diese Änderungen auch in den Modulen übernommen werden.</p>
+				</div>
+			</div>
+   			</section>';
 
 
+$content .= '<section class="rex-page-section">
+             <div class="panel panel-default">
+    			<header class="panel-heading">
+					<div class="panel-title">vNews aufsetzen</div>
+				</header>
+         			<div class="panel-body">
+					<p>Bevor vNews genutzt werden kann, müssen einige Vorkehrungen getroffen werden.</p>
+						<dl>
+							<dt>YRewrite aufsetzen</dt>
+							<dd>YRewrite -> Domain aufsetzen, im URL darf nicht <code>www.domain.com/index.php?article_id=1</code> stehen sondern <code>www.domain.com/startseite</code> </dd>
+							<p></p>
+							<dt>Artikel für Modul "0743 - vNews Ausgabe Detailansicht" erstellen/auswählen</dt>
+							<dd>Erstellung des Platzhalter-Artikel für das Modul und platzieren des Moduls. Es muss noch keine Einstellungen im Modul selber vorgenommen werden.</dd>
+							<p></p>
+							<dt>Url-Rewerite für Platzhalter-Artikel erstellen.</dt>
+							<dd>Im <a href="index.php?page=url/generate">Url-AddOn</a> der von "vNews" erstellten Eintrag auswählen und im ersten Feld den Platzhalter-Artikel verlinken.</dd>
+							<p></p>
+							<dt>Artikel für Modul "0742 - vNews Ausgabe Artikelliste" erstellen/asuwählen</dt>
+							<dd>Artikel auswählen und Modul platzieren. Bei Option "Detailansicht-Artikel" den Plathzahlter-Artikel des Moduls "0743 - vNews Ausgabe Detailansicht" auswählen. Weitere Einstellungen können Sie später vornehmen.</dd>
+						</dl>
+						<dl>
+							
+						</dl>
+					</div>
+				</div>
+   			</section>';
+$content .= '';
+$content .= '';
+$content .= '';
 
 $fragment = new rex_fragment();
-$fragment->setVar('title', Anleitung);
-$fragment->setVar('body', $content, false);
-echo $fragment->parse('core/page/section.php');
+$fragment->setVar( 'title', Anleitung );
+$fragment->setVar( 'body', $content, false );
+echo $fragment->parse( 'core/page/section.php' );
